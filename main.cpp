a622d7b2b36f7b4335a4922a9b82088bc94cd708
Paul566 <arkhipov.pa@phystech.edu>
2025-07-08 16:32:15 +0200

    add Graph and Tree classes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <vector>
+#include "Graph.h"
+#include "Tree.h"
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::vector<std::vector<int>> adj_list;
+    adj_list.emplace_back(std::vector<int>{1, 2, 3});
+    adj_list.emplace_back(std::vector<int>{0, 3});
+    adj_list.emplace_back(std::vector<int>{0, 3});
+    adj_list.emplace_back(std::vector<int>{0, 1, 2});
+
+    Graph graph(adj_list);
+    graph.PrintGraph();
+
     return 0;
 }
