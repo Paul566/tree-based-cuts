5118786ea6a65992aecb5e066f11403bc659ee7e
Paul566 <arkhipov.pa@phystech.edu>
2025-07-09 12:58:59 +0200

    add tree structure initialization

--- Tree.cpp
+++ Tree.cpp
@@ -1,8 +1,12 @@
 #include "Tree.h"
 
+#include <queue>
+
 
 Tree::Tree(const std::vector<std::vector<int> > &_adj_list) {
     adj_list = _adj_list;
+
+    InitializeTree();
 }
 
 Tree::Tree(std::vector<std::pair<int, int>> _edge_list) {
@@ -12,8 +16,36 @@ Tree::Tree(std::vector<std::pair<int, int>> _edge_list) {
         adj_list[edge.first].push_back(edge.second);
         adj_list[edge.second].push_back(edge.first);
     }
+
+    InitializeTree();
 }
 
 Tree::Tree() {
-    adj_list = std::vector<std::vector<int>>();
+
+}
+
+void Tree::InitializeTree() {
+    // initialize the children and parent vectors
+    // parent of the root is -1
+
+    parent = std::vector<int>(adj_list.size());
+    children = std::vector<std::vector<int>>(adj_list.size(), std::vector<int>());
+
+    std::queue<int> queue;
+    std::vector<bool> visited(adj_list.size(), false);
+    queue.push(root);
+    visited[root] = true;
+    parent[root] = -1;
+    while (!queue.empty()) {
+        int node = queue.front();
+        queue.pop();
+        visited[node] = true;
+        for (int neighbor : adj_list[node]) {
+            if (!visited[neighbor]) {
+                queue.push(neighbor);
+                children[node].push_back(neighbor);
+                parent[neighbor] = node;
+            }
+        }
+    }
 }
--- Tree.h
+++ Tree.h
@@ -4,13 +4,19 @@
 
 class Tree {
     public:
+        int root;
         std::vector<std::vector<int>> adj_list;
+        std::vector<std::vector<int>> children;
+        std::vector<int> parent;
 
         explicit Tree(const std::vector<std::vector<int>> &_adj_list);
 
         explicit Tree(std::vector<std::pair<int, int>> _edge_list);
 
         Tree();
+
+    private:
+        void InitializeTree();
 };
 
 
