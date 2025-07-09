1f867416a6c8ecbd7827de5c9fe4d2492360a56b
Paul566 <arkhipov.pa@phystech.edu>
2025-07-09 13:37:36 +0200

    add subtree sizes and heavy parents

--- Tree.cpp
+++ Tree.cpp
@@ -1,13 +1,8 @@
 #include "Tree.h"
 
 #include <queue>
+#include <stack>
 
-
-Tree::Tree(const std::vector<std::vector<int> > &_adj_list) {
-    adj_list = _adj_list;
-
-    InitializeTree();
-}
 
 Tree::Tree(std::vector<std::pair<int, int>> _edge_list) {
     const int n = static_cast<int>(_edge_list.size()) + 1;
@@ -17,14 +12,15 @@ Tree::Tree(std::vector<std::pair<int, int>> _edge_list) {
         adj_list[edge.second].push_back(edge.first);
     }
 
-    InitializeTree();
+    InitializeTreeStructure();
+    InitializeSubtreeSizes();
 }
 
 Tree::Tree() {
 
 }
 
-void Tree::InitializeTree() {
+void Tree::InitializeTreeStructure() {
     // initialize the children and parent vectors
     // parent of the root is -1
 
@@ -47,5 +43,51 @@ void Tree::InitializeTree() {
                 parent[neighbor] = node;
             }
         }
+    }
+}
+
+void Tree::InitializeSubtreeSizes() {
+    subtree_sizes = std::vector<int>(adj_list.size(), 1);
+
+    std::stack<int> postorder_stack;
+
+    std::stack<int> stack;
+    stack.push(root);
+    while (!stack.empty()) {
+        int node = stack.top();
+        stack.pop();
+        postorder_stack.push(node);
+        for (int child : children[node]) {
+            stack.push(child);
+        }
+    }
+
+    while (!postorder_stack.empty()) {
+        int node = postorder_stack.top();
+        postorder_stack.pop();
+        for (int child : children[node]) {
+            subtree_sizes[node] += subtree_sizes[child];
+        }
+    }
+}
+
+void Tree::InitializeHeavyParents() {
+    std::queue<int> queue;
+    queue.push(root);
+    while (!queue.empty()) {
+        int node = queue.front();
+        queue.pop();
+        if (children[node].empty()) {
+            continue;
+        }
+        int max_subtree_size = 0;
+        int heaviest_child = 0;
+        for (int i = 0; i < static_cast<int>(children[node].size()); ++i) {
+            if (subtree_sizes[children[node][i]] > max_subtree_size) {
+                max_subtree_size = subtree_sizes[children[node][i]];
+                heaviest_child = children[node][i];
+            }
+        }
+        heavy_parent[heaviest_child] = node;
     }
 }
--- Tree.h
+++ Tree.h
@@ -9,14 +9,19 @@ class Tree {
         std::vector<std::vector<int>> children;
         std::vector<int> parent;
 
-        explicit Tree(const std::vector<std::vector<int>> &_adj_list);
-
         explicit Tree(std::vector<std::pair<int, int>> _edge_list);
 
         Tree();
 
     private:
-        void InitializeTree();
+        std::vector<int> subtree_sizes; // subtree size of a leaf vertex is 1
+        std::vector<bool> heavy_parent; // for a vertex v, heavy_parent[v] is true if (v, parent[v]) is a heavy edge
+
+        void InitializeTreeStructure();
+
+        void InitializeSubtreeSizes();
+
+        void InitializeHeavyParents();
 };
 
 
